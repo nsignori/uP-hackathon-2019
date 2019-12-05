2f24f57961e98572a7a81a78fcc9514cca932952
Nicholas Signori <nicholas.signori@gmail.com>
2019-12-04 22:45:25 -0500

    initialize main

---
+++ hackathon/hackathon/main.c
@@ -0,0 +1,18 @@
+/*
+ * hackathon.c
+ *
+ * Created: 12/4/2019 10:42:35 PM
+ * Author : Nick
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
